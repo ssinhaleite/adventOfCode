97de3c9d7697dd2de0c4c71cf060fccaa76cf86a
Vanessa Leite <vanessa@ini.uzh.ch>
2023-12-07 21:33:51 +0100

    WIP - result not yet submitted

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -6,62 +6,63 @@
 #include <set>
 #include <math.h> 
 
-int main()
-{
+int main() {
 
-    std::ifstream input;
-    input.open("input2.txt");
+	std::ifstream input;
+	input.open("input.txt");
 
-    std::string line;
-    int sum = 0;
+	std::string line;
+	int sum = 0;
 
-    while (std::getline(input, line))
-    {
-        // line always starts with "Card X: " remove it from line
-        std::string delimiter = ":";
-        std::string position = line.substr(0, line.find(delimiter));
-        line.erase(0, position.size() + 1);
+	while (std::getline(input, line)) {
+		// line always starts with "Card X: " remove it from line
+		std::string delimiter = ":";
+		std::string position = line.substr(0, line.find(delimiter));
+		line.erase(0, position.size() + 1);
 
-        std::cout << line;
+		delimiter = "|";
+		std::string numbers = line.substr(0, line.find(delimiter));
+		line.erase(0, numbers.size() + 1);
 
-        delimiter = "|";
-        std::string numbers = line.substr(0, line.find(delimiter));
-        line.erase(0, numbers.size() + 1);
-        
-        std::stringstream ss;
-        ss << numbers;
+		std::set<int> winnerNumbers;
+		std::string number = "";
+		for (unsigned int i = 0; i <= numbers.size(); i++) {
+			if (numbers[i] == ' ') {
+				if (number.size() > 0) {
+					int winnerNumber = std::stoi(number);
+					winnerNumbers.insert(winnerNumber);
+					number.clear();
+				}
+			} else {
+				number += numbers[i];
+			}
+		}
 
-        std::set<int> winnerNumbers;
-        while (ss.tellp() == std::streampos(0))
-        {
-            int winnerNumber;
-            ss >> winnerNumber;
-            std::cout << winnerNumber << std::endl;
-            winnerNumbers.insert(winnerNumber);
-        }
+		int numbersFound = 0;
+		number = "";
+		for (unsigned int i = 0; i <= line.size(); i++) {
+			if (line[i] == ' ') {
+				if (number.size() > 0) {
+					int myNumber = std::stoi(number);
+					if (winnerNumbers.find(myNumber) != winnerNumbers.end()) {
+						numbersFound++;
+						number.clear();
+					} else {
+						number.clear();
+					}
+				}
+			} else {
+				number += line[i];
+			}
+		}
 
-        ss << line;
+		if (numbersFound != 0) {
+			sum += pow(2, numbersFound - 1);
+		}
+	}
 
-        int numbersFound = 0;
+	input.close();
+	std::cout << "answer: " << sum << std::endl;
 
-        while (ss.tellp() == std::streampos(0))
-        {
-            int number;
-            ss >> number;
-            std::cout << number << std::endl;
-
-            if(winnerNumbers.find(number) != winnerNumbers.end())
-            {
-                numbersFound++;
-            }
-        }
-
-        sum += pow(2, numbersFound - 1);
-
-    }
-
-    input.close();
-    std::cout << "answer: " << sum << std::endl;
-
-    return 0;
+	return 0;
 }
